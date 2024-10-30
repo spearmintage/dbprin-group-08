fb2e90f6bd930fbf100bddf7d6acfb6a9c413ee1
Owen <up2199179@myport.ac.uk>
2024-10-30 14:01:10 +0000

    modified:   dbprin_group_08.sql: added attributes to student table

--- dbprin_group_08.sql
+++ dbprin_group_08.sql
@@ -1,4 +1,19 @@
-CREATE TABLE student();
+CREATE TABLE student(
+student_id SERIAL PRIMARY KEY,
+student_email VARCHAR(50),
+student_mobile VARCHAR(15),
+student_first_name VARCHAR(30),
+student_middle_name VARCHAR(30),
+student_last_name VARCHAR(30),
+student_title VARCHAR(4),
+student_country_code VARCHAR(3),
+student_landline VARCHAR(15),
+student_addr1 VARCHAR(50),
+student_addr2 VARCHAR(50),
+student_city VARCHAR(30),
+student_postcode VARCHAR(12),
+student_date_of_birth DATE
+);
 
 CREATE TABLE teacher();
 
