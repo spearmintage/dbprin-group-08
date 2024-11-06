ff20791b9d0b8dd10560a4fd5dceea4792cc26a0
Bradley Smith <smithbradley913@gmail.com>
2024-11-06 15:51:35 +0000

    assigned tables for everyone to complete

--- dbprin_group_08.sql
+++ dbprin_group_08.sql
@@ -2,17 +2,18 @@ DROP DATABASE IF EXISTS dbprin_group_08;
 CREATE DATABASE dbprin_group_08;
 \c dbprin_group_08
 
--- TODO: go through every attribute and consider:
--- CHECK (between a value, fits regex)
--- NOT NULL
--- UNIQUE
+-- Note:
+-- if any changes differ from the ERD,
+-- Make a note within the table.
 
+-- Owen
 CREATE TABLE department (
     dept_id SERIAL PRIMARY KEY,
     dept_name VARCHAR(50),
     description TEXT
 );
 
+-- Owen
 CREATE TABLE course (
     course_id SERIAL PRIMARY KEY,
     dept_id INT NOT NULL,
@@ -23,6 +24,7 @@ CREATE TABLE course (
     FOREIGN KEY (dept_id) REFERENCES department(dept_id)
 );
 
+-- Bradley
 CREATE TABLE subject (
     subject_id SERIAL PRIMARY KEY,
     subject_name VARCHAR(50),
@@ -30,6 +32,7 @@ CREATE TABLE subject (
     subject_description TEXT
 );
 
+-- Bradley
 CREATE TABLE course_subject (
     course_id INT NOT NULL,
     subject_id INT NOT NULL,
@@ -40,6 +43,7 @@ CREATE TABLE course_subject (
     PRIMARY KEY (course_id, subject_id)
 );
 
+-- Steph
 CREATE TABLE staff (
     staff_id SERIAL PRIMARY KEY,
     dept_id INT NOT NULL,
@@ -62,6 +66,7 @@ CREATE TABLE staff (
     FOREIGN KEY (branch_id) REFERENCES branch(branch_id)
 );
 
+-- Steph
 CREATE TABLE staff_assignment (
     staff_assignment_id SERIAL PRIMARY KEY,
     branch_manager_id INT NOT NULL,
@@ -75,6 +80,7 @@ CREATE TABLE staff_assignment (
     FOREIGN KEY (staff_id) REFERENCES staff(staff_id),
 );
 
+-- Bradley
 CREATE TABLE course_staff (
     course_id INT NOT NULL,
     staff_id INT NOT NULL,
@@ -85,12 +91,14 @@ CREATE TABLE course_staff (
     PRIMARY KEY (course_id, staff_id)
 );
 
+-- Steph
 CREATE TABLE role (
     role_id SERIAL PRIMARY KEY,
     role_name VARCHAR(50),
     role_description TEXT
 );
 
+-- Steph
 CREATE TABLE staff_role (
     course_id INT NOT NULL,
     staff_id INT NOT NULL,
@@ -101,83 +109,100 @@ CREATE TABLE staff_role (
     PRIMARY KEY (course_id, staff_id)
 );
 
+-- Steph
 CREATE TABLE staff_availability (
 
 );
 
+-- Steph
 CREATE TABLE branch (
 
 );
 
+-- Steph
 CREATE TABLE room (
 
 );
 
+-- Steph
 CREATE TABLE facility (
 
 );
 
+-- Steph
 CREATE TABLE room_facility (
 
 );
 
+-- Owen
 CREATE TABLE student(
-student_id SERIAL PRIMARY KEY,
-student_email VARCHAR(50),
-student_mobile VARCHAR(15),
-student_first_name VARCHAR(30),
-student_middle_name VARCHAR(30),
-student_last_name VARCHAR(30),
-student_title VARCHAR(4),
-student_country_code VARCHAR(3),
-student_landline VARCHAR(15),
-student_addr1 VARCHAR(50),
-student_addr2 VARCHAR(50),
-student_city VARCHAR(30),
-student_postcode VARCHAR(12),
-student_date_of_birth DATE
+    student_id SERIAL PRIMARY KEY,
+    student_email VARCHAR(50),
+    student_mobile VARCHAR(15),
+    student_first_name VARCHAR(30),
+    student_middle_name VARCHAR(30),
+    student_last_name VARCHAR(30),
+    student_title VARCHAR(4),
+    student_country_code VARCHAR(3),
+    student_landline VARCHAR(15),
+    student_addr1 VARCHAR(50),
+    student_addr2 VARCHAR(50),
+    student_city VARCHAR(30),
+    student_postcode VARCHAR(12),
+    student_date_of_birth DATE
 );
 
+-- Owen
 CREATE TABLE emergency_contact (
 
 );
 
+-- Owen
 CREATE TABLE health_condition (
 
 );
 
+-- Owen
 CREATE TABLE student_health_condition (
 
 );
 
+-- Bradley
 CREATE TABLE evaluation_session (
 
 );
 
+-- Bradley
 CREATE TABLE assignment (
 
 );
 
+-- Bradley
 CREATE TABLE student_assignment (
 
 );
 
+-- Bradley
 CREATE TABLE session (
 
 );
 
+-- Bradley
 CREATE TABLE session_student (
 
 );
 
+-- Bradley
 CREATE TABLE session_staff (
 
 );
 
+-- Owen
 CREATE TABLE enrolment (
 
 );
 
+-- Owen
 CREATE TABLE student_payment (
 
 );
